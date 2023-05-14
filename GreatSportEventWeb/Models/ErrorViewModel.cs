84097236ea0e670cf30f25aac05ecfb5dc2571f5
Danil Fedoseev <fs159@mail.ru>
2023-05-14 17:24:13 +0500

    Initial commit

---
+++ GreatSportEventWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GreatSportEventWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
