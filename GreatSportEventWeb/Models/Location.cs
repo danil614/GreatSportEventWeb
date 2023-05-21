3005281592c0fe94b2696ccffb508e39d7b7ca1a
Danil Fedoseev <fs159@mail.ru>
2023-05-21 19:48:13 +0500

    Поработал с валидацией

--- GreatSportEventWeb/Models/Location.cs
+++ GreatSportEventWeb/Models/Location.cs
@@ -9,35 +9,36 @@ public class Location : AsSerializable
 {
     [Key] [Column("location_id")] public int Id { get; set; }
 
-    [Required(ErrorMessage = "Необходимо ввести название.")]
+    [Required(ErrorMessage = "Поле является обязательным.")]
     [Display(Name = "Название")]
     [Column("location_name")]
     [StringLength(60, ErrorMessage = "Текст должен быть меньше 60 символов.")]
     public string Name { get; set; } = null!;
 
     [Display(Name = "Город")]
-    [Required(ErrorMessage = "Необходимо выбрать город.")]
+    [Required(ErrorMessage = "Поле является обязательным.")]
     [Column("city_id")]
     public long CityId { get; set; }
     
-    public virtual City? City { get; set; } = null!;
+    public virtual City? City { get; set; }
 
-    [Required(ErrorMessage = "Необходимо ввести адрес.")]
+    [Required(ErrorMessage = "Поле является обязательным.")]
     [Column("address")]
     [Display(Name = "Адрес")]
     [StringLength(60, ErrorMessage = "Текст должен быть меньше 60 символов.")]
     public string Address { get; set; } = null!;
     
     [Display(Name = "Тип")]
-    [Required(ErrorMessage = "Необходимо выбрать тип.")]
+    [Required(ErrorMessage = "Поле является обязательным.")]
     [Column("location_type_id")]
     public int TypeId { get; set; }
     
-    public virtual Type? Type { get; set; } = null!;
+    public virtual Type? Type { get; set; }
 
     [Display(Name = "Вместимость")]
-    [Range(1, int.MaxValue, ErrorMessage = "Значение вместимости должно быть больше 0 и меньше 2147483647.")]
-    [Required(ErrorMessage = "Необходимо ввести вместимость.")]
+    [Range(1, int.MaxValue, ErrorMessage = "Число должно быть больше 0 и меньше 2147483647.")]
+    [RegularExpression("^[0-9]+$", ErrorMessage = "Число должно быть целым и положительным.")]
+    [Required(ErrorMessage = "Поле является обязательным.")]
     [Column("capacity")]
     public int Capacity { get; set; }
 
