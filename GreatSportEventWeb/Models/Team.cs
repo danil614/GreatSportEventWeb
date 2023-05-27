ee40ccc27a1ba4a84c1cdddc74ffd2088bc10316
Danil Fedoseev <fs159@mail.ru>
2023-05-27 21:46:35 +0500

    Начал добавлять модель команд

---
+++ GreatSportEventWeb/Models/Team.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GreatSportEventWeb.Models;
+
+[Table("Teams")]
+public class Team
+{
+    [Key]
+    [Column("team_id")]
+    public int Id { get; set; }
+
+    [Required]
+    [Column("location_id")]
+    public int LocationId { get; set; }
+
+    [Required]
+    [Column("team_name")]
+    public string Name { get; set; }
+
+    [Required]
+    [Column("come_from")]
+    public string ComeFrom { get; set; }
+
+    [Required]
+    [Column("rating")]
+    public int Rating { get; set; }
+
+    [Column("description")]
+    public string Description { get; set; }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
